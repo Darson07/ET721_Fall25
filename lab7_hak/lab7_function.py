0faa37857528d73a62c7c32a0aadcb3f982ae3e5
Darson07 <darson.hak50@student.qcc.cuny.edu>
2025-10-15 00:18:45 -0400

    Completed Lab #7

--- lab7_hak/lab7_function.py
+++ lab7_hak/lab7_function.py
@@ -1,7 +1,7 @@
 '''
 Darson Hak
 Function File
-09/29/2025
+October 14th, 2025
 Lab #7: 
 '''
 def testing():
@@ -10,8 +10,80 @@ def testing():
 # EXAMPLE #1: Read File
 def read_data(filename):
     # pipe a text line in a file with a Python code
-    fileuser = open(filename, 'r')
+    with open(filename, "r") as file1:
+        filecontent = file1.read()
+        print(filecontent)
 
-    # use a loop to go over each line in fileuser
-    for each in fileuser:
-        print(each)
+    # check if the file is closed. If it is closed, it should return True
+    print(f"Is the file closed? {file1.closed}")
+
+# EXAMPLE #2: Reading specific portion of a file 
+def read_up(filename):
+    with open(filename, "r") as file1:
+        # read the first 30 characters
+        print(file1.read(30))
+        # read the next 5 characters
+        print(file1.read(5))
+
+# EXAMPLE #3: Readlines
+def read_readline(filename):
+    with open(filename, "r") as file1:
+        # read up to 30 characters of the first line
+        print(file1.readline(30))
+        # continues reading next line up to 5 characters
+        print(file1.readline(5))
+
+# EXAMPLE #4: Readlines
+def read_all(filename):
+    with open(filename, "r") as file1:
+        print(file1.readlines())
+
+# EXAMPLE #5: Loop Through a Readlines File
+def read_each(filename):
+    with open(filename, "r") as file1:
+        filelines = file1.readlines()
+
+        # Loop through each item in 'filelines'
+        for eachline in filelines:
+            print(eachline.strip())
+            # strip() removes the newline character \n
+
+# EXAMPLE #6: Create a New File
+def new_file(filename):
+    with open(filename, "w") as file:
+        file.write("Python Basics for data analysis\n")
+        file.write("Darson Hak")
+
+# EXAMPLE #7: Append Data into an Existing File
+from datetime import datetime
+def stamp_date(filename):
+    with open(filename, "a") as file:
+        file.write(f"\n\n{datetime.now()}")
+
+# LAB EXERCISE
+def email_read(filename, report):
+    try:
+        count_yahoo = 0
+        count_gmail = 0
+        count_hotmail = 0
+
+        with open(filename, "r") as file1:
+            for eachline in file1:
+                email = eachline.lower().strip()
+
+                if "@yahoo.com" in email:
+                    count_yahoo += 1
+
+                elif "gmail.com" in email:
+                    count_gmail += 1 
+
+                elif "hotmail.com" in email:
+                    count_hotmail += 1
+
+        with open(report, "w") as file:
+            file.write(f"yahoo = {count_yahoo}\n")
+            file.write(f"gmail = {count_gmail}\n")
+            file.write(f"hotmail = {count_hotmail}\n")
+    
+    except FileNotFoundError:
+        print("'user_email.txt' was not found")
--- lab7_hak/lab7_main.py
+++ lab7_hak/lab7_main.py
@@ -1,10 +1,31 @@
 '''
 Darson Hak
 Lab #7: Accessing Data in a File
-09/29/2025
+October 14th, 2025
 '''
 from lab7_function import *
 
 testing()
 print("\n----- Example #1: Reading a File -----")
 read_data("phrases.txt")
+
+print("\n----- Example #2: Reading Specific Portion of a File -----")
+read_up("phrases.txt")
+
+print("\n----- Example #3: Reading Specific Portion using readline -----")
+read_readline("phrases.txt")
+
+print("\n----- Example #4: Reading Specific Portion using readlines -----")
+read_all("phrases.txt")
+
+print("\n----- Example #5: Loop Through Each Line -----")
+read_each("phrases.txt")
+
+print("\n----- Example #6: Creating a New File -----")
+new_file("hak.txt")
+
+print("\n----- Example #7: Append Data Into a New File -----")
+stamp_date("hak.txt")
+
+print("\n----- LAB EXERCISE -----")
+email_read("user_email.txt", "reportemail.txt")
